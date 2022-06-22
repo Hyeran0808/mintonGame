7b194e877db1f6ffa70f06588f92c708fe7b9844
Hyeran0808 <gpfsks0808@naver.com>
2022-06-22 18:40:18 +0900

    6/22

--- app/src/main/java/com/example/mintongame/ResultActivity.java
+++ app/src/main/java/com/example/mintongame/ResultActivity.java
@@ -1,14 +1,54 @@
 package com.example.mintongame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class ResultActivity extends AppCompatActivity {
+
+    Fragment fragment1, fragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, fragment1).commit();
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+                if(position == 1){
+                    selected = fragment1;
+                } else {
+                    selected = fragment2;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
