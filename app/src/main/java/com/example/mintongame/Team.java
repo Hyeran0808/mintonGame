58afb945c892479963a1a20662c5e289c068deef
Hyeran0808 <gpfsks0808@naver.com>
2022-06-24 17:24:13 +0900

    6/23

---
+++ app/src/main/java/com/example/mintongame/Team.java
@@ -0,0 +1,13 @@
+package com.example.mintongame;
+
+public class Team {
+    String name, gen, age, grade;
+
+    public Team(String name, String gen, String age, String grade){
+        this.name = name;
+        this.gen = gen;
+        this.age = age;
+        this.grade = grade;
+    }
+
+}
